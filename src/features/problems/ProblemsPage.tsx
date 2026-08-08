e4f209a40a4f522fe7375d5c97ffb4718a405caa
jcandyn <jos.candy@gmail.com>
2026-08-08 09:08:23 -0400

    Fix review drawer close behavior

--- src/features/problems/ProblemsPage.tsx
+++ src/features/problems/ProblemsPage.tsx
@@ -14,6 +14,7 @@ const ProblemsPage = () => {
 
   useEffect(() => {
     if (!problemToOpen) {
+      if (openedFromUrl.current) selectProblem(null);
       openedFromUrl.current = null;
       return;
     }
